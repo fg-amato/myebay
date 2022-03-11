8055b68701236cb07a22bf8ed7b5d63184174e9c
flavio.g.amato <flavio.g.amato@gmail.com>
2022-03-11 13:08:16 +0100

    Funzione di visualizzazione utente inserita

--- src/main/java/it/prova/myebay/web/servlet/utente/ExecuteVisualizzaUtenteServlet.java
+++ src/main/java/it/prova/myebay/web/servlet/utente/ExecuteVisualizzaUtenteServlet.java
@@ -1,41 +1,54 @@
 package it.prova.myebay.web.servlet.utente;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ExecuteVisualizzaUtenteServlet
- */
-@WebServlet("/ExecuteVisualizzaUtenteServlet")
+import org.apache.commons.lang3.math.NumberUtils;
+
+import it.prova.myebay.model.Utente;
+import it.prova.myebay.service.MyServiceFactory;
+
+@WebServlet("/admin/ExecuteVisualizzaUtenteServlet")
 public class ExecuteVisualizzaUtenteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ExecuteVisualizzaUtenteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String idUtenteParam = request.getParameter("idUtente");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		if (!NumberUtils.isCreatable(idUtenteParam)) {
+			// qui ci andrebbe un messaggio nei file di log costruito ad hoc se fosse attivo
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore.");
+			request.getRequestDispatcher("ExecuteListUtenteServlet").forward(request, response);
+			return;
+		}
+		// se sono qui i valori sono ok quindi posso creare l'oggetto da inserire
+		// occupiamoci delle operazioni di business
+		try {
+			Utente utenteInstance = MyServiceFactory.getUtenteServiceInstance()
+					.caricaSingoloElementoConRuoli(Long.parseLong(idUtenteParam));
+			if (utenteInstance == null) {
+				request.setAttribute("errorMessage", "Elemento non trovato.");
+				request.getRequestDispatcher("ExecuteListFilmServlet?operationResult=NOT_FOUND").forward(request,
+						response);
+				return;
+			}
+
+			request.setAttribute("show_utente_attr", utenteInstance);
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore.");
+			request.getRequestDispatcher("ExecuteListUtenteServlet").forward(request, response);
+			return;
+		}
+
+		// andiamo ai risultati
+		request.getRequestDispatcher("../utente/show.jsp").forward(request, response);
 	}
 
 }
